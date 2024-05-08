917741dc34e8959cef50166d3573e25682f00b64
bgdnrvsky <raevskijb@gmail.com>
2024-05-08 23:37:01 +0200

    Added function to get all minecraft versions (except snapshots and betas)

--- src/fetchers.rs
+++ src/fetchers.rs
@@ -1,10 +1,15 @@
+use std::str::FromStr;
+
 use anyhow::anyhow;
 use loading::{Loading, Spinner};
 use reqwest as rq;
+use semver::Version;
 use serde::Deserialize;
+use serde_json::Value;
 
 pub const TOKEN: &str = "$2a$10$bL4bIL5pUWqfcO7KQtnMReakwtfHbNKh6v1uTpKlzhwoueEJQnPnm";
 const GAMES_LIST_URL: &str = "https://api.curseforge.com/v1/games"; // https://github.com/fn2006/PollyMC/wiki/CurseForge-Workaround
+const MINECRAFT_VERSIONS_LIST_URL: &str = "https://mc-versions-api.net/api/java";
 
 #[derive(Debug, Deserialize)]
 struct GamesList {
@@ -68,4 +73,42 @@ pub fn get_minecraft_id() -> anyhow::Result<usize> {
     loading.end();
 
     minecraft_id
+}
+
+pub fn get_minecraft_versions() -> anyhow::Result<Vec<Version>> {
+    let loading = Loading::new(Spinner::default());
+
+    loading.info(format!(
+        "Retrieving Minecraft's versions from {url}",
+        url = MINECRAFT_VERSIONS_LIST_URL
+    ));
+
+    loading.text("Downloading");
+
+    let req = rq::blocking::Request::new(
+        rq::Method::GET,
+        rq::Url::parse(MINECRAFT_VERSIONS_LIST_URL)?,
+    );
+
+    let client = rq::blocking::Client::new();
+    let response = client.execute(req)?;
+
+    let json: Value = serde_json::from_str(&response.text()?)?;
+
+    json.get("result")
+        .ok_or(anyhow!("No versions are present in response's JSON"))?
+        .as_array()
+        .ok_or(anyhow!("Result in JSON response was not an array"))?
+        .iter()
+        .map(|value| {
+            value
+                .as_str()
+                .ok_or(anyhow!(
+                    "One of the values in 'result' key of JSON was not a string"
+                ))
+                .and_then(|value| {
+                    Version::from_str(value).map_err(|e| anyhow!("Failed to parse version: {e}"))
+                })
+        })
+        .collect()
 }
