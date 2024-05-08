2b49c6cc983236d98aff388ceb2378c8deed0df6
bgdnrvsky <raevskijb@gmail.com>
2024-05-08 23:44:44 +0200

    Write tests for fetchers
    Change retrieving Minecraft versions to use VersionReq since it doesn't
    require exactly 3 numeric components
    Disable logging while testing

--- src/fetchers.rs
+++ src/fetchers.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use anyhow::anyhow;
 use loading::{Loading, Spinner};
 use reqwest as rq;
-use semver::Version;
+use semver::VersionReq;
 use serde::Deserialize;
 use serde_json::Value;
 
@@ -46,13 +46,16 @@ impl GameEntry {
 }
 
 pub fn get_minecraft_id() -> anyhow::Result<usize> {
+    #[cfg(not(test))]
     let loading = Loading::new(Spinner::default());
 
+    #[cfg(not(test))]
     loading.info(format!(
         "Retrieving Minecraft's ID from {url}",
         url = GAMES_LIST_URL
     ));
 
+    #[cfg(not(test))]
     loading.text("Decoding game entries");
 
     let mut req = rq::blocking::Request::new(rq::Method::GET, rq::Url::parse(GAMES_LIST_URL)?);
@@ -70,19 +73,23 @@ pub fn get_minecraft_id() -> anyhow::Result<usize> {
         .map(GameEntry::get_id)
         .ok_or(anyhow!("Minecraft was not found in the list of games"));
 
+    #[cfg(not(test))]
     loading.end();
 
     minecraft_id
 }
 
-pub fn get_minecraft_versions() -> anyhow::Result<Vec<Version>> {
+pub fn get_minecraft_versions() -> anyhow::Result<Vec<VersionReq>> {
+    #[cfg(not(test))]
     let loading = Loading::new(Spinner::default());
 
+    #[cfg(not(test))]
     loading.info(format!(
         "Retrieving Minecraft's versions from {url}",
         url = MINECRAFT_VERSIONS_LIST_URL
     ));
 
+    #[cfg(not(test))]
     loading.text("Downloading");
 
     let req = rq::blocking::Request::new(
@@ -94,6 +101,9 @@ pub fn get_minecraft_versions() -> anyhow::Result<Vec<Version>> {
     let response = client.execute(req)?;
 
     let json: Value = serde_json::from_str(&response.text()?)?;
+
+    #[cfg(not(test))]
+    loading.end();
 
     json.get("result")
         .ok_or(anyhow!("No versions are present in response's JSON"))?
@@ -107,8 +117,23 @@ pub fn get_minecraft_versions() -> anyhow::Result<Vec<Version>> {
                     "One of the values in 'result' key of JSON was not a string"
                 ))
                 .and_then(|value| {
-                    Version::from_str(value).map_err(|e| anyhow!("Failed to parse version: {e}"))
+                    VersionReq::from_str(value).map_err(|e| anyhow!("Failed to parse version: {e}"))
                 })
         })
         .collect()
+}
+
+#[cfg(test)]
+mod fetchers_test {
+    use crate::fetchers::{get_minecraft_id, get_minecraft_versions};
+
+    #[test]
+    fn minecraft_id() {
+        assert!(get_minecraft_id().is_ok());
+    }
+
+    #[test]
+    fn minecraft_versions() {
+        assert!(get_minecraft_versions().is_ok());
+    }
 }
