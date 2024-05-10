7220f8977e7b8d45859c14f4832fd75118f6332a
bgdnrvsky <raevskijb@gmail.com>
2024-05-10 17:22:02 +0200

    Clear main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-use crate::loader::Loader;
-
 mod config;
 mod fetchers;
 mod loader;
 
 fn main() -> anyhow::Result<()> {
-    println!(
-        "{:#?}",
-        toml::from_str::<Loader>(
-            r#"
-            kind = "forge"
-            version = "47.2.2"
-"#,
-        )
-    );
-
     Ok(())
 }
