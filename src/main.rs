70b11873cd76feb8712865d4766d8f0879b8c849
bgdnrvsky <raevskijb@gmail.com>
2024-05-11 17:22:20 +0200

    Implement Fabric mod loading from JAR file
    Add new field of incompatible mods for `Mod` structure

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ mod config;
 mod fetchers;
 mod loader;
 
-use std::{collections::HashMap, path::Path};
+use std::path::Path;
 
 use anyhow::{anyhow, Context};
 use jars::{jar, Jar, JarOptionBuilder};
 use loader::Loaders;
 use serde::{Deserialize, Serialize};
-use toml::{Table, Value};
+use serde_json::Value as JsonValue;
+use toml::{Table, Value as TomlValue};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 struct ModDep {
@@ -16,7 +17,13 @@ struct ModDep {
     id: String,
     #[serde(rename = "versionRange")]
     versions: String,
-    mandatory: bool
+    mandatory: bool,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct ModIncomp {
+    id: String,
+    versions: String,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -24,6 +31,7 @@ struct Mod {
     id: String,
     version: String, // TODO: Use custom version struct
     dependencies: Vec<ModDep>,
+    incompatibilities: Option<Vec<ModIncomp>>,
 }
 
 impl Mod {
@@ -58,8 +66,9 @@ impl Mod {
             .as_str()
             .ok_or(anyhow!("The key `version` was not a string"))?;
 
-        let dependencies: anyhow::Result<Vec<ModDep>> =
-            table.get("dependencies").ok_or(anyhow!("Forge's mod spec didn't have the key `dependencies`"))?
+        let dependencies: anyhow::Result<Vec<ModDep>> = table
+            .get("dependencies")
+            .ok_or(anyhow!("Forge's mod spec didn't have the key `dependencies`"))?
             .as_table()
             .ok_or(anyhow!("The key `dependencies` in forge mod's spec is expected to be a table"))?
             .get(mod_id)
@@ -67,18 +76,71 @@ impl Mod {
             .as_array()
             .ok_or(anyhow!("Dependencies in forge mod's spec weren't in an array"))?
             .iter()
-            .map(|value| Value::try_into(value.clone()).map_err(|_| anyhow!("error")))
+            .map(|value| TomlValue::try_into(value.clone()).with_context(|| anyhow!("Couldn't transform dependency entry")))
             .collect();
 
         Ok(Self {
             id: mod_id.to_owned(),
             version: mod_version.to_owned(),
             dependencies: dependencies?,
+            incompatibilities: None,
         })
     }
 
     fn load_fabric(jar: Jar) -> anyhow::Result<Self> {
-        todo!()
+        let content = jar.files.get("fabric.mod.json").ok_or(anyhow!(
+            "No fabric.mod.json in jar file while processing fabric mod"
+        ))?;
+
+        let json = serde_json::from_slice::<JsonValue>(content)?;
+        let object = json.as_object().ok_or(anyhow!(
+            "fabric.mod.json file is expected to be an object (map)"
+        ))?;
+
+        let mod_id = object
+            .get("id")
+            .ok_or(anyhow!("Fabric mod's spec didn't contain the key `id`"))?
+            .as_str()
+            .ok_or(anyhow!("Fabric mod's id wasn't a string"))?;
+
+        let mod_version = object
+            .get("version")
+            .ok_or(anyhow!(
+                "Fabric mod's spec didn't contain the key `version`"
+            ))?
+            .as_str()
+            .ok_or(anyhow!("Fabric mod's version wasn't a string"))?;
+
+        let dependencies: Vec<ModDep> = object
+            .get("depends")
+            .ok_or(anyhow!("No key `depends` in fabric mod's spec"))?
+            .as_object()
+            .ok_or(anyhow!("The key `depends` wasn't an object (map)"))?
+            .into_iter()
+            .map(|(id, versions)| ModDep {
+                id: id.to_string(),
+                versions: versions.to_string(),
+                mandatory: true,
+            })
+            .collect();
+
+        let incompatibilities: Option<Vec<ModIncomp>> = object
+            .get("breaks")
+            .and_then(|value| value.as_object())
+            .map(|object| {
+                object.into_iter().map(|(id, versions)| ModIncomp {
+                    id: id.to_string(),
+                    versions: versions.to_string(),
+                })
+            })
+            .map(Iterator::collect);
+
+        Ok(Self {
+            id: mod_id.to_string(),
+            version: mod_version.to_string(),
+            dependencies,
+            incompatibilities,
+        })
     }
 
     pub fn from_jar(path: impl AsRef<Path>) -> anyhow::Result<Self> {
@@ -104,12 +166,7 @@ impl Mod {
 }
 
 fn main() -> anyhow::Result<()> {
-    let jar = jar(
-        "btp.jar",
-        JarOptionBuilder::builder().keep_meta_info().build(),
-    )?;
-
-    println!("{:#?}", Mod::load_forge(jar));
+    println!("{:#?}", Mod::from_jar("sodium.jar"));
 
     Ok(())
 }
