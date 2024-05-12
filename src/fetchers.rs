08cbc797592491667ca84a5590c1b995b67182ba
bgdnrvsky <raevskijb@gmail.com>
2024-05-12 21:58:19 +0200

    Disable search limit

--- src/fetchers.rs
+++ src/fetchers.rs
@@ -211,7 +211,6 @@ impl Fetcher {
                 .copied()?;
 
             querys.append_pair("gameId", &format!("{id}"));
-            querys.append_pair("pageSize", "50");
             querys.append_pair(
                 "classId",
                 &format!(
