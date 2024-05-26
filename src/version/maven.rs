e754c23dca1816d95c58f1295ab4657a2952e112
bgdnrvsky <raevskijb@gmail.com>
2024-05-26 13:48:44 +0200

    Implement `VersionRange` parsing from string
    Added unit tests for comparators

--- src/version/maven.rs
+++ src/version/maven.rs
@@ -1,9 +1,99 @@
+use anyhow::anyhow;
 use nom::{
+    bytes::complete::tag,
     character::complete::{alpha1, digit1, one_of},
     combinator::map_res,
     multi::separated_list1,
-    IResult, Parser,
+    sequence::{delimited, preceded, separated_pair, terminated},
+    Finish, IResult, Parser,
 };
+use std::str::FromStr;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Comparator {
+    Exact(Version),
+    Greater(Version),
+    GreaterEq(Version),
+    Less(Version),
+    LessEq(Version),
+    BetweenInclusive(Version, Version),
+    BetweenUnInclusive(Version, Version),
+}
+
+impl Comparator {
+    fn parse(s: &str) -> IResult<&str, Self> {
+        macro_rules! single {
+            ($op:literal x $cl:literal) => {
+                delimited(tag($op), Version::parse, tag($cl))
+            };
+        }
+
+        macro_rules! double {
+            ($op:literal x y $cl:literal) => {
+                preceded(
+                    tag($op),
+                    terminated(
+                        separated_pair(Version::parse, tag(","), Version::parse),
+                        tag($cl),
+                    ),
+                )
+            };
+        }
+
+        single!("(," x "]")
+            .map(Self::LessEq)
+            .or(single!("(," x ")").map(Self::Less))
+            .or(single!("[" x "]").map(Self::Exact))
+            .or(single!("[" x ",)").map(Self::GreaterEq))
+            .or(single!("(" x ",)").map(Self::Greater))
+            .or(double!("[" x y "]").map(|(x, y)| Self::BetweenInclusive(x, y)))
+            .or(double!("(" x y ")").map(|(x, y)| Self::BetweenUnInclusive(x, y)))
+            .parse(s)
+    }
+}
+
+impl FromStr for Comparator {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match Self::parse(s).finish() {
+            Ok(("", item)) => Ok(item),
+            Ok((rest, item)) => {
+                eprintln!("The Comparator was parsed, but remaining input is left: `{rest}`");
+                Ok(item)
+            }
+            Err(e) => Err(anyhow!("Error while parsing Comparator: {e}")),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct VersionRange {
+    comparators: Vec<Comparator>,
+}
+
+impl VersionRange {
+    fn parse(s: &str) -> IResult<&str, Self> {
+        separated_list1(tag(","), Comparator::parse)
+            .map(|comparators| Self { comparators })
+            .parse(s)
+    }
+}
+
+impl FromStr for VersionRange {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match Self::parse(s).finish() {
+            Ok(("", item)) => Ok(item),
+            Ok((rest, item)) => {
+                eprintln!("The VersionRange was parsed, but remaining input is left: `{rest}`");
+                Ok(item)
+            }
+            Err(e) => Err(anyhow!("Error while parsing Comparator: {e}")),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum VersionItem {
@@ -36,6 +126,52 @@ impl Version {
 
     fn new(items: Vec<VersionItem>) -> Self {
         Self { items }
+    }
+}
+
+impl FromStr for Version {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match Self::parse(s).finish() {
+            Ok(("", item)) => Ok(item),
+            Ok((rest, item)) => {
+                eprintln!("The Version was parsed, but remaining input is left: `{rest}`");
+                Ok(item)
+            }
+            Err(e) => Err(anyhow!("Error while parsing Version: {e}")),
+        }
+    }
+}
+
+#[cfg(test)]
+mod comparators {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        // TODO
+        // assert_eq!(
+        //     Comparator::from_str("1.0").ok(),
+        //     Some(Comparator::GreaterEq(Version::from_str("1.0").unwrap()))
+        // );
+
+        assert!(Comparator::from_str("(,1.0]").is_ok());
+        assert!(Comparator::from_str("(,1.0)").is_ok());
+        assert!(Comparator::from_str("[1.0]").is_ok());
+        assert!(Comparator::from_str("[1.0]").is_ok());
+        assert!(Comparator::from_str("[1.0,)").is_ok());
+        assert!(Comparator::from_str("(1.0,)").is_ok());
+        assert!(Comparator::from_str("(1.0,2.0)")
+            .is_ok_and(|item| matches!(item, Comparator::BetweenUnInclusive(_, _))));
+        assert!(Comparator::from_str("[1.0,2.0]")
+            .is_ok_and(|item| matches!(item, Comparator::BetweenInclusive(_, _))));
+    }
+
+    #[test]
+    fn multiple() {
+        assert!(VersionRange::from_str("(,1.0],[1.2,)").is_ok());
+        assert!(VersionRange::from_str("(,1.1),(1.1,)").is_ok());
     }
 }
 
