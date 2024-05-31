a8b4eeb0f74ece5bf3d2c077356fc53e03674b03
bgdnrvsky <raevskijb@gmail.com>
2024-06-01 01:47:37 +0200

    Prepare universal version system

--- src/version/mod.rs
+++ src/version/mod.rs
@@ -1,1 +1,19 @@
 pub mod maven;
+
+use maven::Version as ForgeVersion;
+use semver::Version as FabricVersion;
+
+use maven::VersionRange as ForgeVersionRange;
+use semver::VersionReq as FabricVersionRange;
+
+#[derive(Debug, Clone)]
+pub enum SingleVersion {
+    Fabric(FabricVersion),
+    Forge(ForgeVersion),
+}
+
+#[derive(Debug, Clone)]
+pub enum ManyVersions {
+    Fabric(FabricVersionRange),
+    Forge(ForgeVersionRange),
+}
