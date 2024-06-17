b8c39258254353e93e8bcc21f34eb42986f72037
bgdnrvsky <raevskijb@gmail.com>
2024-06-17 20:37:40 +0200

    Relocate imports

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod config;
+pub mod fetchers;
+pub mod loader;
+pub mod minecraft_mod;
+pub mod version;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,3 @@
-mod config;
-mod fetchers;
-mod loader;
-mod minecraft_mod;
-mod version;
-
 fn main() -> anyhow::Result<()> {
     Ok(())
 }
