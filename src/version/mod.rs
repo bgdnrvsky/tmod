001e28b95c0b7647b377862ca5727f54200453f5
bgdnrvsky <raevskijb@gmail.com>
2024-06-17 19:51:03 +0200

    Implement `Display` trait for custom version structs

--- src/version/mod.rs
+++ src/version/mod.rs
@@ -1,5 +1,7 @@
 pub mod maven;
 
+use std::fmt::Display;
+
 use maven::Version as ForgeVersion;
 use semver::Version as FabricVersion;
 
@@ -14,9 +16,27 @@ pub enum SingleVersion {
     Forge(ForgeVersion),
 }
 
+impl Display for SingleVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SingleVersion::Fabric(version) => write!(f, "{version}"),
+            SingleVersion::Forge(version) => write!(f, "{version}"),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
 #[serde(untagged)]
 pub enum ManyVersions {
     Fabric(FabricVersionRange),
     Forge(ForgeVersionRange),
+}
+
+impl Display for ManyVersions {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ManyVersions::Fabric(version) => write!(f, "{version}"),
+            ManyVersions::Forge(version) => write!(f, "{version}"),
+        }
+    }
 }
