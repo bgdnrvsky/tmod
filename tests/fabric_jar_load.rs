f60a8de644a26618c1c783616828b9419ba30652
bgdnrvsky <raevskijb@gmail.com>
2024-07-10 13:48:56 +0200

    integ_tests: Add fabric mod loading from jar file test

--- tests/fabric_jar_load.rs
+++ tests/fabric_jar_load.rs
@@ -1,9 +1,36 @@
-use tmod::minecraft_mod::Mod;
+use std::str::FromStr;
+
+use tmod::{
+    minecraft_mod::Mod,
+    version::{
+        fabric::{Version, VersionReq},
+        MultiVersion, SingleVersion,
+    },
+};
 
 #[test]
-#[ignore]
 fn load_fabric() -> anyhow::Result<()> {
-    let _sodium = Mod::from_jar("tests/sodium.jar")?;
+    let sodium = Mod::from_jar("tests/sodium.jar")?;
+
+    assert_eq!(sodium.id(), "sodium");
+
+    assert_eq!(
+        sodium.version(),
+        SingleVersion::Fabric(Version::from_str("0.5.8")?)
+    );
+
+    assert!(!sodium.dependencies().is_empty());
+    assert!(!sodium.incompatibilities().is_empty());
+
+    assert_eq!(
+        sodium.minecraft_version_needed(),
+        MultiVersion::Fabric(VersionReq::any())
+    );
+
+    assert_eq!(
+        sodium.loader_version_needed(),
+        MultiVersion::Fabric(VersionReq::from_str(">=0.12.0")?)
+    );
 
     Ok(())
 }
