50e5199fb27d2080e61bf04dbba96ae5a4882b53
bgdnrvsky <raevskijb@gmail.com>
2024-07-16 01:29:56 +0200

    custom_semver: Implement ordering for single version

--- src/version/fabric/version.rs
+++ src/version/fabric/version.rs
@@ -21,6 +21,22 @@ pub struct Version {
     build: Option<BuildMetadata>,
 }
 
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Version {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.major
+            .cmp(&other.major)
+            .then(self.minor.cmp(&other.minor))
+            .then(self.patch.cmp(&other.patch))
+            .then(self.pre.cmp(&other.pre))
+    }
+}
+
 impl Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}.{}.{}", self.major, self.minor, self.patch)?;
