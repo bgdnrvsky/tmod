ea6a0b4e6ec38ca4f30c1ca9f6603083e129bad1
bgdnrvsky <raevskijb@gmail.com>
2024-08-01 11:23:18 +0200

    Splitting jar loading into its own module
    Implemented Fabric mod loading

---
+++ src/jar_mod/fabric.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+use anyhow::Context;
+use jars::Jar;
+use serde::Deserialize;
+
+use crate::version::fabric::{Version, VersionReq};
+
+#[derive(Debug, Clone)]
+pub struct FabricMod {
+    id: String,
+    version: Version,
+    loader_version_needed: VersionReq,
+    minecraft_version_needed: VersionReq,
+    // Key: mod slug
+    dependencies: HashMap<String, VersionReq>,
+    // Key: mod slug
+    incompatibilities: HashMap<String, VersionReq>,
+}
+
+impl TryFrom<Jar> for FabricMod {
+    type Error = anyhow::Error;
+
+    fn try_from(jar: Jar) -> Result<Self, Self::Error> {
+        let content = jar
+            .files
+            .get("fabric.mod.json")
+            .context("No fabric.mod.json in jar file while processing fabric mod")?;
+
+        let fabric_json = serde_json::from_slice::<FabricJson>(content)?;
+        let mut dependencies = fabric_json.depends;
+
+        let incompatibilities = fabric_json.breaks;
+
+        let loader_version_needed = dependencies
+            .remove("fabricloader")
+            .unwrap_or_else(VersionReq::any);
+
+        let minecraft_version_needed = dependencies
+            .remove("minecraft")
+            .unwrap_or_else(VersionReq::any);
+
+        Ok(Self {
+            id: fabric_json.id,
+            version: fabric_json.version,
+            loader_version_needed,
+            minecraft_version_needed,
+            dependencies,
+            incompatibilities,
+        })
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct FabricJson {
+    id: String,
+    version: Version,
+    #[serde(default)]
+    depends: HashMap<String, VersionReq>,
+    #[serde(default)]
+    breaks: HashMap<String, VersionReq>,
+}
--- src/jar_mod/mod.rs
+++ src/jar_mod/mod.rs
@@ -1,1 +1,2 @@
 pub mod forge;
+pub mod fabric;
