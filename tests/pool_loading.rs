ce1a2850e82cebc4a01cdddf1e25c87106604d7e
bgdnrvsky <raevskijb@gmail.com>
2024-08-02 12:38:04 +0200

    Add more to pool's integration test

--- tests/pool_loading.rs
+++ tests/pool_loading.rs
@@ -3,7 +3,13 @@ use tmod::pool::pool::Pool;
 
 #[test]
 fn read_pool() -> anyhow::Result<()> {
-    let _pool = Pool::new("tests/test_pool").context("Reading pool")?;
+    let pool = Pool::new("tests/test_pool").context("Reading pool")?;
+
+    let remotes = pool.remotes();
+    assert!(remotes.contains_key("foo"));
+    assert!(remotes.contains_key("bar"));
+
+    assert!(pool.locals().is_empty());
 
     Ok(())
 }
