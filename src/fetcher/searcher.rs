8e4fe6420b12fd76a9fdb17a8f930e2a6b6cb2d3
bgdnrvsky <raevskijb@gmail.com>
2024-08-07 12:01:25 +0200

    Remove unneeded test

--- src/fetcher/searcher.rs
+++ src/fetcher/searcher.rs
@@ -114,17 +114,4 @@ mod tests {
 
         Ok(())
     }
-
-    #[test]
-    fn mod_by_slug() -> anyhow::Result<()> {
-        let searcher = Searcher::new();
-
-        let alexs_mobs = searcher.search_mod_by_name("alexs-mobs")?;
-        assert_eq!(alexs_mobs.count(), 1);
-
-        let jei = searcher.search_mod_by_name("jei")?;
-        assert_eq!(jei.count(), 1);
-
-        Ok(())
-    }
 }
