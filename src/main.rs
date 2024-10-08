316bbf72f455e2f185316e548603835c468b1d41
bgdnrvsky <raevskijb@gmail.com>
2024-10-08 13:38:29 +0200

    Inline initial silencing of the searcher [no ci]

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,6 @@ use cli::Cli;
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-
-    {
-        let mut searcher = Cli::get_searcher_mut();
-        searcher.set_silent(cli.quiet);
-    }
-
+    Cli::get_searcher_mut().set_silent(cli.quiet);
     cli.run()
 }
