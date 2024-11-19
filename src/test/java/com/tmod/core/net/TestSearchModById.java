2562c60660d69eeaa8ba6d963f580296b0f30f83
bgdnrvsky <raevskijb@gmail.com>
2024-11-19 02:07:20 +0100

    Rename test to `TestSearchModById`

--- src/test/java/com/tmod/core/net/TestSearchModById.java
+++ src/test/java/com/tmod/core/net/TestSearchModById.java
@@ -8,14 +8,19 @@ import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CurseForgeGetTest {
-
+class TestSearchModById {
     @Test
-    void searchModById() throws URISyntaxException, IOException, InterruptedException {
+    void searchExistingMinecraftMod() throws URISyntaxException, IOException, InterruptedException {
         Mod jei = TmodClient.searchModById(238222);
         assertEquals("jei", jei.slug());
 
         Mod geckolib = TmodClient.searchModById(388172);
         assertEquals("geckolib", geckolib.slug());
+    }
+
+    @Test
+    void searchExistingNonMinecraftMod() throws URISyntaxException, IOException, InterruptedException {
+        Mod sims_mod = TmodClient.searchModById(694283);
+        assertNull(sims_mod);
     }
 }
