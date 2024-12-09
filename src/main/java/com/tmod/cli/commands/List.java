39316ca4fdf1f9629c315af1e328550aa2260a1c
bgdnrvsky <raevskijb@gmail.com>
2024-12-09 19:16:06 +0100

    cli: Implement `List` command

--- src/main/java/com/tmod/cli/commands/List.java
+++ src/main/java/com/tmod/cli/commands/List.java
@@ -1,18 +1,44 @@
 package com.tmod.cli.commands;
 
 import com.tmod.cli.App;
+import com.tmod.core.models.Mod;
+import com.tmod.core.net.TmodClient;
+import com.tmod.core.repo.Mapper;
+import com.tmod.core.repo.Repository;
+import java.nio.file.Path;
 import picocli.CommandLine;
 
-@CommandLine.Command(
-        name = "list",
-        description = "List the mods in the repo"
-)
-public class List implements Runnable  {
+@CommandLine.Command(name = "list", description = "List the mods in the repo")
+public class List implements Runnable {
+
     @CommandLine.ParentCommand
     private App parent;
 
     @Override
     public void run() {
-        System.out.println("Listing mods in the repository");
+        try {
+            Mapper mapper = new Mapper(Path.of(parent.getRepoPath()));
+            Repository repository = mapper.read();
+
+            int i = 1;
+
+            for (String slug : repository.getManuallyAdded()) {
+                Mod mod = TmodClient.searchModBySlug(slug);
+
+                System.out.println(
+                    String.format(
+                        "%d. %s(%d): %s",
+                        i,
+                        mod.name(),
+                        mod.id(),
+                        mod.summary()
+                    )
+                );
+
+                i += 1;
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
