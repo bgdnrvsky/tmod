d2eceb2ce77516aa13479f7957a15c8268485bea
bgdnrvsky <raevskijb@gmail.com>
2024-12-09 19:39:26 +0100

    cli: Implement `Remove` command

--- src/main/java/com/tmod/cli/commands/Remove.java
+++ src/main/java/com/tmod/cli/commands/Remove.java
@@ -1,27 +1,52 @@
 package com.tmod.cli.commands;
 
 import com.tmod.cli.App;
+import com.tmod.core.repo.Mapper;
+import com.tmod.core.repo.Repository;
+import java.nio.file.Path;
+import java.util.Set;
 import picocli.CommandLine;
 
-import java.util.Set;
-
 @CommandLine.Command(
-        name = "remove",
-        description = "Remove one or multiple mods from the repo"
+    name = "remove",
+    description = "Remove one or multiple mods from the repo"
 )
 public class Remove implements Runnable {
+
     @CommandLine.ParentCommand
     private App parent;
 
-    @CommandLine.Parameters(
-        arity = "1..*",
-        paramLabel = "slugs"
-    )
+    @CommandLine.Parameters(arity = "1..*", paramLabel = "slugs")
     private Set<String> removalTargetMods;
 
     @Override
     public void run() {
-        System.out.println("Removing mods from repo");
-        System.out.println(removalTargetMods);
+        try {
+            Mapper mapper = new Mapper(Path.of(parent.getRepoPath()));
+            Repository repository = mapper.read();
+
+            for (String slug : removalTargetMods) {
+                boolean removedFromManuallyAdded = repository
+                    .getManuallyAdded()
+                    .remove(slug);
+                boolean removedFromLocks =
+                    repository.getLocks().remove(slug) != null;
+                boolean removedFromRepo =
+                    removedFromManuallyAdded || removedFromLocks;
+
+                if (!removedFromRepo) {
+                    System.out.println(
+                        String.format(
+                            "The mod '%s' wasn't present in the repo",
+                            slug
+                        )
+                    );
+                }
+            }
+
+            mapper.write(repository);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
