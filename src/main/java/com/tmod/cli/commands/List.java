b2631f0b11be999240ee6206a1b1bc244129b8b6
bgdnrvsky <raevskijb@gmail.com>
2024-12-17 15:28:20 +0100

    feat(cli): Add `slugs` flag in `List`
    Allows to list using mod slug, without searching the mod online and
    using its _display name_

--- src/main/java/com/tmod/cli/commands/List.java
+++ src/main/java/com/tmod/cli/commands/List.java
@@ -8,12 +8,20 @@ import com.tmod.core.repo.Repository;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 
 @CommandLine.Command(name = "list", description = "List the mods in the repo")
 public class List implements Runnable {
 
     @CommandLine.ParentCommand
     private App parent;
+
+    @Option(
+        names = { "-s", "--slugs" },
+        description = "Use slug as mod name",
+        defaultValue = "false"
+    )
+    private boolean useSlug = false;
 
     @Override
     public void run() {
@@ -24,12 +32,18 @@ public class List implements Runnable {
             int i = 1;
 
             for (String slug : repository.getManuallyAdded()) {
-                // TODO: Add option not to search the mod and print the slugs directly
-                Mod mod = TmodClient.searchModBySlug(slug);
+                String modName;
+
+                if (this.useSlug) {
+                    modName = slug;
+                } else {
+                    Mod mod = TmodClient.searchModBySlug(slug);
+                    modName = mod.name();
+                }
 
                 Ansi msg = new Ansi();
 
-                msg.format("%d. ", i).fgBlue().a(mod.name()).fgDefault();
+                msg.format("%d. ", i).fgBlue().a(modName).fgDefault();
 
                 AnsiConsole.out().println(msg);
 
