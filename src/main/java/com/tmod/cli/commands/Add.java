abd14ab10855fc5f3648d756de38a9031da1941b
bgdnrvsky <raevskijb@gmail.com>
2024-12-22 16:19:16 +0100

    cli: Implement `Add` command

--- src/main/java/com/tmod/cli/commands/Add.java
+++ src/main/java/com/tmod/cli/commands/Add.java
@@ -1,8 +1,17 @@
 package com.tmod.cli.commands;
 
 import com.tmod.cli.App;
+import com.tmod.core.models.File;
 import com.tmod.core.models.Mod;
+import com.tmod.core.models.RelationType;
+import com.tmod.core.net.CurseForgeApiGetException;
+import com.tmod.core.net.NoFilesFetchedException;
 import com.tmod.core.net.TmodClient;
+import com.tmod.core.repo.Mapper;
+import com.tmod.core.repo.Repository;
+import com.tmod.core.repo.models.Configuration;
+import com.tmod.core.repo.models.DependencyInfo;
+import java.util.stream.Collectors;
 import picocli.CommandLine;
 
 @CommandLine.Command(
@@ -27,10 +36,51 @@ public class Add implements Runnable {
     )
     private boolean clientOnly = false;
 
+    /**
+     * Adds a mod to the locks registry, as well as all of its dependencies
+     *
+     * @throws NoFilesFetchedException if couldn't find any files for a mod
+     * @throws CurseForgeApiGetException if couldn't perform the GET request
+     */
+    private void recursivelyAddToLocks(Mod mod, Repository repository)
+        throws NoFilesFetchedException, CurseForgeApiGetException {
+        Configuration config = repository.getConfig();
+
+        File modFile = TmodClient.newModFileGetter(mod)
+            .withGameVersion(config.gameVersion())
+            .withModLoader(config.loader())
+            .get();
+
+        java.util.List<Mod> dependencies = modFile
+            .relations()
+            .stream()
+            .filter(
+                relation ->
+                    relation.relationType() ==
+                        RelationType.RequiredDependency ||
+                    relation.relationType() == RelationType.EmbeddedLibrary
+            )
+            .map(relation -> TmodClient.searchModById(relation.modId()))
+            .collect(Collectors.toList());
+
+        DependencyInfo dependencyInfo = new DependencyInfo(
+            modFile.fileDate(),
+            clientOnly,
+            dependencies.stream().map(Mod::slug).collect(Collectors.toList())
+        );
+
+        repository.getLocks().put(mod.slug(), dependencyInfo);
+
+        for (Mod dependency : dependencies) {
+            recursivelyAddToLocks(dependency, repository);
+        }
+    }
+
     @Override
     public void run() {
         try {
-            // Repository repo = Mapper.read(options.getRepositoryPath());
+            Mapper mapper = new Mapper(parent.getRepoPath());
+            Repository repository = mapper.read();
             Mod mod;
 
             try {
@@ -38,6 +88,11 @@ public class Add implements Runnable {
             } catch (NumberFormatException e) {
                 mod = TmodClient.searchModBySlug(target);
             }
+
+            repository.getManuallyAdded().add(mod.slug());
+            recursivelyAddToLocks(mod, repository);
+
+            mapper.write(repository);
 
             System.out.println(mod);
         } catch (Exception e) {
