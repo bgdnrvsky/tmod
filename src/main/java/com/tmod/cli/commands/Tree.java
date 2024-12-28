14c3c69031332a1993b8084369dfbfe218122d39
bgdnrvsky <raevskijb@gmail.com>
2024-12-28 18:51:33 +0100

    cli: Implement `Tree` subcommand

--- src/main/java/com/tmod/cli/commands/Tree.java
+++ src/main/java/com/tmod/cli/commands/Tree.java
@@ -1,18 +1,51 @@
 package com.tmod.cli.commands;
 
 import com.tmod.cli.App;
+import com.tmod.core.repo.Mapper;
+import com.tmod.core.repo.Repository;
+import com.tmod.core.repo.models.DependencyInfo;
+import hu.webarticum.treeprinter.SimpleTreeNode;
+import hu.webarticum.treeprinter.printer.listing.ListingTreePrinter;
+import java.util.Map;
 import picocli.CommandLine;
 
 @CommandLine.Command(
-        name = "tree",
-        description = "Print the tree of added mods and dependencies"
+    name = "tree",
+    description = "Print the tree of added mods and dependencies"
 )
 public class Tree implements Runnable {
+
     @CommandLine.ParentCommand
     private App parent;
 
+    private SimpleTreeNode generateNode(
+        String slug,
+        Map<String, DependencyInfo> locks
+    ) {
+        SimpleTreeNode node = new SimpleTreeNode(slug);
+
+        for (String dependency : locks.get(slug).dependencies()) {
+            node.addChild(generateNode(dependency, locks));
+        }
+
+        return node;
+    }
+
     @Override
     public void run() {
-        System.out.println("Printing mods tree");
+        try {
+            Mapper mapper = new Mapper(parent.getRepoPath());
+            Repository repository = mapper.read();
+
+            SimpleTreeNode root = new SimpleTreeNode("tmod");
+
+            for (String slug : repository.getManuallyAdded()) {
+                root.addChild(generateNode(slug, repository.getLocks()));
+            }
+
+            new ListingTreePrinter().print(root);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
